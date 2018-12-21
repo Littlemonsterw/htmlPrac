a5a1a7834eaede7103fe137ad23a01cd77e64a51
Littlemonsterw <1067533996@qq.com>
2018-12-21 17:08:44 +0800

    add click

--- Sudoku/js/randon.js
+++ Sudoku/js/randon.js
@@ -1,3 +1,9 @@
-<scrpt>
-    
-</scrpt>
+function mOver(obj) {
+	obj.backgroundColor = "#f0ad4e";
+	obj.color = '#fff';
+}
+
+function mOut(obj) {
+	obj.backgroundColor = "#fff";
+	obj.color = '#f0ad4e';
+}
